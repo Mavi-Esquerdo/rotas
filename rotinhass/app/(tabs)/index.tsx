da4499a675b0b4bc50178c7ce58052a4ebda87fa
Mavi-Esquerdo <marciahelenaesquerdo@gmail.com>
2024-08-05 17:47:05 +0000

    sava

---
+++ rotinhass/app/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { View, StyleSheet,  } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+
+  
+});
